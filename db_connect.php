d913dfa1f8501597eff24089793dec581b00b3d6
Nghi Tran <pnghi320@yahoo.com>
2021-12-19 03:13:21 -0700

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,8 +1,8 @@
 <?php
-$host = "localhost";
-$username = "root";
-$user_pass = "root";
-$database_in_use = "jokes";
+$host = "l6glqt8gsx37y4hs.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
+$username = "ww6berbazb1k58tk";
+$user_pass = "ld3mx3i58l0a07hk";
+$database_in_use = "kibbmrdybd033qoz";
 
 $mysqli = new mysqli($host,$username,$user_pass,$database_in_use);
 ?>
